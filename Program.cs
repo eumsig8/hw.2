f19bb3b08eb3e7d552f1205d0b2462aaa831dca3
Mariia <mariia.kulyk@sa.stud.vu.lt>
2025-04-29 19:58:09 +0300

    filled program

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace hw2
         {
             var streamJson = File.ReadAllText("User.json");
             User? jsonDes = JsonSerializer.Deserialize<User>(streamJson);
-            Console.WriteLine("Parsed user = {0}, {1}, {2}, {}", jsonDes.Id, jsonDes.Password, jsonDes.Email, jsonDes.Name);
+            Console.WriteLine("Parsed user = {0}, {1}, {2}, {3}", jsonDes.Id, jsonDes.Password, jsonDes.Email, jsonDes.Name);
             streamJson = File.ReadAllText("Admin.json");
             Admin? admin = JsonSerializer.Deserialize<Admin>(streamJson);
             Console.WriteLine("The admin is = {0}, {1}, {2}, {3}, {4}", admin.Name, admin.Password, admin.Age, admin.Email, admin.Balance);
